8c2f37b88bc45bbbe2189c90b7392dfccd5b119c
Danuel Kubenko <8lastday@gmail.com>
2023-02-09 18:57:24 +0200

    Start

--- src/main/java/com/hillel/kubenko/homeworks/homeworks4/Main.java
+++ src/main/java/com/hillel/kubenko/homeworks/homeworks4/Main.java
@@ -3,5 +3,6 @@ package com.hillel.kubenko.homeworks.homeworks4;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("Hello World");
     }
 }
