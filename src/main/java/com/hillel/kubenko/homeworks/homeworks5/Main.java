ff3a52770a332533fac6e8457e079a51e03faeb3
Danuel Kubenko <8lastday@gmail.com>
2023-02-13 19:20:11 +0200

    Start2

--- src/main/java/com/hillel/kubenko/homeworks/homeworks5/Main.java
+++ src/main/java/com/hillel/kubenko/homeworks/homeworks5/Main.java
@@ -10,8 +10,8 @@ public class Main {
         int ridMin = 12;
         int numLi =  860;
         int numMin = (int) (860 * 1.5);
-        int armyLi = (int) ((numLi * warLi)+(numLi * warLi)+(numLi * warLi));
-        int armyMin = (int) ((numMin * warMin)+(numMin * warMin)+(numMin * warMin));
+        int armyLi = (int) (numLi * warLi+numLi * warLi + numLi * warLi);
+        int armyMin = (int) (numMin * warMin+numMin * warMin + numMin * warMin);
         System.out.print("army Li strength: " +armyLi);
         System.out.print("\narmy Min strength: " +armyMin);
     }
