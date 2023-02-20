ae9721856b90933c3c5da9dd92df440f68ec0b75
Danuel Kubenko <8lastday@gmail.com>
2023-02-20 13:23:41 +0200

    Start3

---
+++ src/main/java/com/hillel/kubenko/homeworks/homeworks7/Main.java
@@ -0,0 +1,26 @@
+package com.hillel.kubenko.homeworks.homeworks7;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = 0;
+        double random = Math.random() * 10;
+        int result = (int) Math.round(random);
+        while (true) {
+            System.out.println("Вгадай число");
+            int number = s.nextInt();
+            if (number == result) {
+                System.out.println("Вгадав");
+                break;
+            } else if (a == 2) {
+                System.out.println("Спроби закінчились");
+                break;
+            } else {
+                System.out.println("Невірно");
+                a++;
+
+            }
+        }
+    }
+}
